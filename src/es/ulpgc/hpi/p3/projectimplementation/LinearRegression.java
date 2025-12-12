371c4bbc764dfc0aad6b912c4772fc9540a8e1fd
lucas <lucasmendozaguancha12@gmail.com>
2025-12-12 12:03:07 +0000

    update LinearRegression class

--- src/es/ulpgc/hpi/p3/projectimplementation/LinearRegression.java
+++ src/es/ulpgc/hpi/p3/projectimplementation/LinearRegression.java
@@ -1,18 +1,44 @@
 package es.ulpgc.hpi.p3.projectimplementation;
 
-public class LinearRegression extends RegressionAnalysis{
+import java.util.Random;
+
+public class LinearRegression extends RegressionAnalysis {
+
     private String independentVariable;
+    private double slope;
+    private double intercept;
 
-    public LinearRegression(String resultSummary, Double executionTime, String dependentVariable, double rSquared, String independentVariable) {
-        super(resultSummary, executionTime, dependentVariable, rSquared);
+
+    public LinearRegression(Database database, AnalysisTopic theme, String dependentVariable, String independentVariable) {
+        super(database, theme, dependentVariable);
         this.independentVariable = independentVariable;
     }
 
-    public String getIndependentVariable() {
-        return independentVariable;
-    }
+    @Override
+    public void runAnalysis() {
+        setStatus(AnalysisStatus.Running);
+        long startTime = System.currentTimeMillis();
 
-    public void setIndependentVariable(String independentVariable) {
-        this.independentVariable = independentVariable;
+        try {
+            Thread.sleep(1000);
+            Random rand = new Random();
+
+            this.slope = 1.5 + rand.nextDouble();
+            this.intercept = 10 + rand.nextDouble();
+            this.rSquared = 0.85;
+
+        } catch (InterruptedException e) {
+            setStatus(AnalysisStatus.Failed);
+            return;
+        }
+
+        long endTime = System.currentTimeMillis();
+
+        String summary = String.format("Linear Model: %s = %.2f * %s + %.2f",
+                dependentVariable, slope, independentVariable, intercept);
+
+        setResultSummary(summary);
+        setExecutionTime((double) (endTime - startTime));
+        setStatus(AnalysisStatus.Completed);
     }
 }
